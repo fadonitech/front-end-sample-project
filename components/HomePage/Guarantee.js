05ec43303f96d53ba4a4454673e67a99557e6017
Henrique Fadoni <henrique@feltapp.com>
2021-02-24 17:11:07 -0300

    Creating Guarantee

---
+++ components/HomePage/Guarantee.js
@@ -0,0 +1,12 @@
+const Guarantee = () => (
+  <div className="guarantee">
+    <h1 className="open-sans">
+      WE OFFER 100% REFUND IF
+    </h1>
+    <h1 className="open-sans">
+      OUR SERVICES EVER FAIL YOUR EXPECTATIONS
+    </h1>
+  </div>
+)
+
+export default Guarantee;
