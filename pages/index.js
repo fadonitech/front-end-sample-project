36b7525dd6b55f364ac61c8eac0591db5aebf88d
Henrique Fadoni <henrique@feltapp.com>
2021-02-25 17:51:00 -0300

    Warning

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head'
+import Image from 'next/image';
+
 import Navbar from '../components/Navbar/Navbar';
 import Header from '../components/HomePage/Header';
 import Credibility from '../components/HomePage/Credibility';
@@ -9,22 +12,50 @@ import Guarantee from '../components/HomePage/Guarantee';
 import ContactUs from '../components/ContactUs/ContactUs';
 import Footer from '../components/Footer/Footer';
 
+const Warning = () => (
+  <div id="warning">
+    <h1 className="adam">
+      Sorry! We are working on the mobile version yet!
+    </h1>
+  </div>
+)
+
+const HomePage = () => (
+  <>
+    <Navbar />
+    <Header />
+    <Credibility />
+    <SubscriptionPlan />
+    <Benefits />
+    <Plans />
+    <Guarantee />
+    <ContactUs />
+  </>
+)
+
 const Home = () => {
+  const [block, setBlock] = useState(false);
+
+  useEffect(() => {
+    setBlock(window.innerWidth < 1100);
+
+    const resizeEvent = window.addEventListener('resize', () => {
+      console.log("works")
+      setBlock(window.innerWidth < 1100);
+    });
+
+    return window.removeEventListener('resize', resizeEvent);
+  }, []);
+
   return (
     <div>
       <Head>
+        <title>FadoniTech</title>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <body>
-        <Navbar />
-        <Header />
-        <Credibility />
-        <SubscriptionPlan />
-        <Benefits />
-        <Plans />
-        <Guarantee />
-        <ContactUs />
-      </body>
+      <main>
+        {block ? <Warning /> : <HomePage />}
+      </main>
       <Footer />
     </div>
   )
