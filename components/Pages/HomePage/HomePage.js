13274ba803ccaec2e3637ae745d5467d0ed2cf13
Henrique Fadoni <henrique@feltapp.com>
2021-06-29 12:02:12 -0300

    Bellow the fold

--- components/Pages/HomePage/HomePage.js
+++ components/Pages/HomePage/HomePage.js
@@ -2,13 +2,23 @@ import { DefaultBtn } from '../../Buttons/Buttons';
 
 export const HomePage = ({ onClick }) => (
   <div id="homepage">
-    <div className="margin-bottom-8">
-      <h1 className="source-sans-semibold title-8">OOPS!</h1>
-      <h1 className="source-sans-semibold title-6 margin-bottom-2">You Caught Us Early!</h1>
-      <h2 className="open-sans-light subtitle-2">CURRENTLY WE ARE WORKING ON THIS PRODUCT AND WILL LAUNCH SOON!</h2>
+    <div className="padding-left-10">
+      <div className="margin-bottom-8 ">
+        <h1 className="source-sans-semibold title-8">OOPS!</h1>
+        <h1 className="source-sans-semibold title-6 margin-bottom-2">You Caught Us Early!</h1>
+        <h2 className="open-sans-light subtitle-2">CURRENTLY WE ARE WORKING ON THIS PRODUCT AND WILL LAUNCH SOON!</h2>
+      </div>
+      <div id="homepage__container--btn">
+        <DefaultBtn content="JOIN WAITING LIST" onClick={onClick} />
+      </div>
     </div>
-    <div id="homepage__container--btn">
-      <DefaultBtn content="JOIN WAITING LIST" onClick={onClick} />
-    </div>
+    {/* <div>
+      <h1>
+        What We Are Building?
+      </h1>
+      <h2>
+
+      </h2>
+    </div> */}
   </div>
 )
