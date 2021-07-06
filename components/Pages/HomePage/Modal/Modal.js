09d7172cdbdf8b59851e43a4e105e879220e3c8e
Henrique Fadoni <henrique@feltapp.com>
2021-07-06 09:36:42 -0300

    Changing Icons

--- components/Pages/HomePage/Modal/Modal.js
+++ components/Pages/HomePage/Modal/Modal.js
@@ -1,9 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
-
 const ModalHeader = ({ secondPage }) => (
   <div>
     <h2 className="open-sans-light text-align-center">
@@ -23,11 +20,11 @@ const TableCell = ({ title = null, val1 = null, val2 = null }) => {
     )
   } else if (val1 !== null) {
     return (
-      <tr className="modal__table--collumn-option-1">{val1 ? (<FontAwesomeIcon icon={faCheckCircle} />) : (<h3 className="source-sans-bold">-</h3>)}</tr>
+      <tr className="modal__table--collumn-option-1">{val1 ? (<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#47B533" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>) : (<h3 className="source-sans-bold">-</h3>)}</tr>
     )
   } else if (val2 !== null) {
     return (
-      <tr className="modal__table--collumn-option-2">{val2 ? (<FontAwesomeIcon icon={faCheckCircle} />) : (<h3 className="source-sans-bold">-</h3>)}</tr>
+      <tr className="modal__table--collumn-option-2">{val2 ? (<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#47B533" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>) : (<h3 className="source-sans-bold">-</h3>)}</tr>
     )
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,12 @@ import { useEffect, useLayoutEffect, useState } from 'react';
 import Navbar from '../components/Blog/BlogNavbar/BlogNavbar';
 import { Modal } from '../components/Pages/HomePage/Modal/Modal';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
-
 import { HomePage } from '../components/Pages/Page';
 import Footer from '../components/Footer/Footer';
 
-const Alert = ({ title, message, icon }) => (
+const Alert = () => (
   <div className="alert-success">
-    <FontAwesomeIcon icon={icon} />
+  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#47B533" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
     <div>
       <h3 className="text-align-center source-sans-bold">SUCCESS!!</h3>
       <p className="text-align-center open-sans-light">Thank You For Subscribing To Our Waiting List!</p>
@@ -84,7 +81,7 @@ const Home = () => {
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <main>
-        {alert.isOpen && <Alert title={alert.title} message={alert.message} icon={faCheckCircle} />}
+        {alert.isOpen && <Alert />}
         {animateModal && <Modal showModal={modal} handleModal={onClick} handleAlert={handleAlert} />}
         <Navbar onClick={onClick} />
         <SubsPlanSelected.Provider value={{}}>
