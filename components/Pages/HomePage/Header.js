cf28094201740b2811c8da82148ecc1d37aa73bc
Henrique Fadoni <henrique@feltapp.com>
2021-10-15 08:45:59 -0300

    HOT FIX - Title On Mobile

--- components/Pages/HomePage/Header.js
+++ components/Pages/HomePage/Header.js
@@ -9,11 +9,11 @@ const Header = () => {
 			<div className='container__left'>
 				<div className='homepage__title'>
 					<div className='homepage__text margin-bottom-8'>
-						<h1 className='source-sans-semibold title-8'>AR Made Easy</h1>
+						<h1 className='source-sans-semibold title-8'>
+							AR Made Easy
+						</h1>
 						<h1 className='source-sans-semibold title-6'>
-							With READY-TO-USE
-							3D Models 
-							For E-commerces
+							With READY-TO-USE 3D Models For E-commerces
 						</h1>
 					</div>
 					<div className='homepage__container--btn'>
