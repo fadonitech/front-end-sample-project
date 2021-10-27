d13a55a721560f3843725ae8e726127a382d99eb
Henrique Fadoni <henrique@feltapp.com>
2021-10-26 22:26:56 -0300

    Adjust - Fix Erros From Aqib

--- components/Pages/HomePage/WhoWeAre.js
+++ components/Pages/HomePage/WhoWeAre.js
@@ -2,7 +2,7 @@ const WhoWeAre = () => (
 	<div className='about-us--section about-us__who-we-are'>
 		<div className='about-us__who-we-are--title'>
 			<h1 className='source-sans-bold'>Who We Are?</h1>
-			<h3 className='open-sans-light title-26'>
+			<h3 className='open-sans-light title-2'>
 				We are a team of passionated people aimed to improve the ways we do
 				business online
 			</h3>
@@ -11,15 +11,15 @@ const WhoWeAre = () => (
 			<div className='about-us__who-we-are--image'>
 				<img src='./image/henrique-fadoni.png' />
 				<div className='about-us__who-we-are--image--text'>
-					<h3 className='open-sans-light title-26'>Henry Fadoni</h3>
-					<h3 className='open-sans-light title-26'>CEO / Founder</h3>
+					<h3 className='open-sans-light title-2'>Henry Fadoni</h3>
+					<h3 className='open-sans-light title-2'>CEO / Founder</h3>
 				</div>
 			</div>
 			<div className='about-us__who-we-are--image'>
 				<img src='./image/aqib-hussain.jpg' />
 				<div className='about-us__who-we-are--image--text'>
-					<h3 className='open-sans-light title-26'>Aqib Hussain</h3>
-					<h3 className='open-sans-light title-26'>Software Developer</h3>
+					<h3 className='open-sans-light title-2'>Aqib Hussain</h3>
+					<h3 className='open-sans-light title-2'>Software Developer</h3>
 				</div>
 			</div>
 		</div>
