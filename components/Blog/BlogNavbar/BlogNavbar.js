8518ccf347310505ce1229a60cd356b23536388f
Henrique Fadoni <henrique@feltapp.com>
2021-11-19 16:14:22 -0300

    Re-styling Navbar

--- components/Blog/BlogNavbar/BlogNavbar.js
+++ components/Blog/BlogNavbar/BlogNavbar.js
@@ -21,10 +21,10 @@ const BlogWebNavbar = () => (
 					<Link href='/about-us'>About Us</Link>
 				</li>
 				<li>
-          <Link href="/blog">
-            Blog
-          </Link>
-        </li>
+					<Link href="/blog">
+						Blog
+					</Link>
+				</li>
 			</ul>
 
 			<Link href='/subscribe'>
@@ -45,22 +45,18 @@ const BlogMobilebNavbar = () => {
 		<>
 			<div className={`navbar-mobile__bg-${btnActive ? 'show' : 'hide'}`}>
 				<Link href='/'>
-					<img
-						src='/logo-black.png'
-						height={65.6}
-						width={63}
-						alt='Software Development'
-						style={{
-							marginTop: '1rem',
-						}}
-					/>
+					<div className="navbar-mobile__icon">
+						<img
+							src='/logo-white.png'
+							alt='Software Development'
+						/>
+					</div>
 				</Link>
 			</div>
 
 			<button
 				className={`
-          navbar-mobile--hamburger hamburger hamburger--collapse ${
-						btnActive && 'is-active'
+          navbar-mobile--hamburger hamburger hamburger--collapse ${btnActive && 'is-active'
 					}
         `}
 				onClick={onClick}
@@ -79,8 +75,18 @@ const BlogMobilebNavbar = () => {
 				</Link>
 				<ul>
 					<li className='open-sans-light'>
+						<Link href='/' onClick={onClick}>
+							Home
+						</Link>
+					</li>
+					<li className='open-sans-light'>
 						<Link href='/about-us' onClick={onClick}>
 							About Us
+						</Link>
+					</li>
+					<li className='open-sans-light'>
+						<Link href='/blog' onClick={onClick}>
+							Blog
 						</Link>
 					</li>
 				</ul>
@@ -93,14 +99,15 @@ const BlogMobilebNavbar = () => {
 };
 
 const BlogNavbar = () => {
+	const resolution = 600;
 	const [isPhone, setIsPhone] = useState(false);
 
 	const validateScreen = () => {
-		setIsPhone(window.innerWidth <= 500);
+		setIsPhone(window.innerWidth <= resolution);
 	};
 
 	useEffect(() => {
-		setIsPhone(window.innerWidth <= 500);
+		setIsPhone(window.innerWidth <= resolution);
 		const resizeEvent = window.addEventListener('resize', validateScreen);
 		return window.removeEventListener('resize', resizeEvent);
 	}, []);
