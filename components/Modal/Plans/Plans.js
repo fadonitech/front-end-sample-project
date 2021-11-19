868d443268e745e141535743344d9edc50a0a9f6
Henrique Fadoni <henrique@feltapp.com>
2021-11-19 15:06:00 -0300

    Adapting GetInvited

--- components/Modal/Plans/Plans.js
+++ components/Modal/Plans/Plans.js
@@ -1,15 +1,44 @@
 import ModalPlan from './Plan';
 
-const ModalPlans = ({ plan, setPlan }) => {
+const ModalPlans = ({ plan, setPlan, warning, setWarning }) => {
   const onClick = (event) => {
+    setWarning({
+      ...warning,
+      plan: false
+    });
     setPlan(event.target.id)
   }
 
   return (
     <div>
-      <ModalPlan id="annual" plan={plan} onClick={onClick} title="Annual" discount={true} subtext="Billed yearly at $479.88" price={39} />
-      <ModalPlan id="monthly" plan={plan} onClick={onClick} title="Monthly" subtext="Cancel at anytime" price={49} />
-      <ModalPlan id="trial" plan={plan} onClick={onClick} title="14 Day Trial" price="FREE" last={true} />
+      <ModalPlan
+        id="annual"
+        plan={plan}
+        onClick={onClick}
+        title="Annual"
+        discount={true}
+        subtext="Billed yearly at $479.88"
+        price={39}
+        warning={warning.plan}
+      />
+      <ModalPlan
+        id="monthly"
+        plan={plan}
+        onClick={onClick}
+        title="Monthly"
+        subtext="Cancel at anytime"
+        price={49}
+        warning={warning.plan}
+      />
+      <ModalPlan
+        id="trial"
+        plan={plan}
+        onClick={onClick}
+        title="14 Day Trial"
+        price="FREE"
+        last={true}
+        warning={warning.plan}
+      />
     </div>
   )
 }
