7400749ad5359cc26e75c4c4fb6cd6849c99c838
Henrique Fadoni <henrique@feltapp.com>
2021-11-28 11:30:37 -0300

    Installing Google Tracking

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -80,6 +80,7 @@ const AboutUsPage = () => {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+						
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
             });
