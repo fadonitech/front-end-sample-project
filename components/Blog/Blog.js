b99b134dd8ae6abfcc91634de3c27d8616269ae7
Henrique Fadoni <henrique@feltapp.com>
2021-11-28 21:18:28 -0300

    Adding Load Animation

--- components/Blog/Blog.js
+++ components/Blog/Blog.js
@@ -4,10 +4,18 @@ import { LIST_BLOG_POSTS } from '../../GraphQL/Queries';
 
 import BlogHeader from './BlogHeader';
 import BlogArticles from './BlogArticles';
+import { LoadingAnimation } from '../../components/Loading/Loading';
 
 const BlogContent = () => {
   const [articles, setArticles] = useState();
   const { error, loading, data } = useQuery(LIST_BLOG_POSTS)
+  let renderComp = (
+    <div>
+      <h1 id="blog-title" className="open-sans">The Blog</h1>
+      {articles && <BlogHeader article={articles[0]} />}
+      {articles && <BlogArticles articles={articles} />}
+    </div>
+  )
 
   useEffect(() => {
     if (data) {
@@ -15,13 +23,21 @@ const BlogContent = () => {
     }
   }, [data])
 
+  if (loading) {
+    renderComp = (
+      <LoadingAnimation />
+    )
+  } else if (error) {
+    renderComp = (
+      <h2 className='source-sans-light title-6'>
+        Ups! Your connection is unstable.
+      </h2>
+    )
+  }
+
   return (
-    <div className="blog-page">
-      <div>
-        <h1 id="blog-title" className="open-sans">The Blog</h1>
-        {articles && <BlogHeader article={articles[0]}/>}
-        {articles && <BlogArticles articles={articles}/>}
-      </div>
+    <div className={`blog-page${loading || error ? '--loading' : ''}`}>
+      {renderComp}
     </div>
   )
 }
