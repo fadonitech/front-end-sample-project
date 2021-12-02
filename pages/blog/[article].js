7ca385a28ac4a42a8af71bd4d73466a92bb1b1b7
Henrique Fadoni <henrique@feltapp.com>
2021-12-02 14:11:23 -0300

    fix - deploy url

--- pages/blog/[article].js
+++ pages/blog/[article].js
@@ -9,92 +9,79 @@ import Navbar from '../../components/Blog/BlogNavbar/BlogNavbar';
 import Footer from '../../components/Footer/Footer';
 
 
-const Article = ({
-  post: {
-    id = '',
-    title = '',
-    description = '',
-    author = '',
-    time = 0,
-    date = '',
-    content = '',
-    image = '',
+const Article = () => {
+  const route = useRouter();
+  const [articles, setArticles] = useState({
+    id: '',
+    title: '',
+    description: '',
+    author: '',
+    time: 0,
+    date: '',
+    content: '',
+    image: '',
+  });
+  const {
+    id,
+    title,
+    description,
+    author,
+    time,
+    date,
+    content,
+    image,
+  } = articles;
+
+  const fetchData = async () => {
+    const newId = route.pathname.split('-').reverse()[0];
+
+    const { data } = await client.query(
+      {
+        query: FIND_BLOG_POST,
+        variables: { newId }
+      }
+    )
+
+    setArticles(data.findBlogPost);
   }
-}) => (
-  <div>
-    <Head>
-      <title>FadoniTech | {title}</title>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
-      <meta name="robots" content="index, follow" />
-      <meta name="description" content={description} />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-    </Head>
-    <main>
-      <Navbar />
-      <div className="blogArticlePage">
-        <div className="blogArticlePage__content">
-          <div className="blogArticlePage__headline">
-            <h1 className="open-sans">
-              {title}
-            </h1>
-            <h2 className="open-sans">
-              {date}
-            </h2>
-          </div>
-          <div className="blogArticlePage__text">
-            <img src={image} alt="Software Development" />
-            <div dangerouslySetInnerHTML={{ __html: content }}></div>
+
+  useEffect(() => {
+    fetchData();
+  }, [articles]);
+
+  return (
+    <div>
+      <Head>
+        <title>FadoniTech | {title}</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
+        <meta name="robots" content="index, follow" />
+        <meta name="description" content={description} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <main>
+        <Navbar />
+        <div className="blogArticlePage">
+          <div className="blogArticlePage__content">
+            <div className="blogArticlePage__headline">
+              <h1 className="open-sans">
+                {title}
+              </h1>
+              <h2 className="open-sans">
+                {date}
+              </h2>
+            </div>
+            <div className="blogArticlePage__text">
+              <img src={image} alt="Software Development" />
+              <div dangerouslySetInnerHTML={{ __html: content }}></div>
+            </div>
           </div>
         </div>
-      </div>
-      <Footer />
-    </main>
-  </div>
-);
-
-export async function getStaticPaths() {
-  const { data } = await client.query(
-    {
-      query: LIST_BLOG_POSTS
-    }
-  )
-
-  const paths = data.blogPosts.map(post => ({
-    params: { article: `${post.title.split(' ').join('-')}-${post.id}` },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  }
-}
-
-export async function getStaticProps({ params }) {
-  const id = params.article.split('-').reverse()[0];
-
-  const { data } = await client.query(
-    {
-      query: FIND_BLOG_POST,
-      variables: { id }
-    }
-  )
-
-  if (data) {
-    return {
-      props: {
-        post: data.findBlogPost
-      }
-    }
-  } else {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      }
-    }
-  }
+        <Footer />
+      </main>
+    </div>
+  );
 }
 
 export default Article;
